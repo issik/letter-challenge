0343c06c25f93bf1dec815f9b35703db48046503
isaac bustos <isaac.youthboard@futureindesign.org>
2018-01-05 10:30:43 -0700

    generaly psudo

---
+++ brainteaser.rb
@@ -0,0 +1,19 @@
+require 'pry'
+     
+    def contact_input
+        puts "type out anything and i will reply with the ascii code for the charcaters in order"
+            letters = gets.strip
+            letter = [].ord
+
+         if letters.length > 0
+            array << letters
+
+        puts " #{letters}"
+          end
+        end
+  end
+  
+  
+ # gets input
+ #each piece of aray is parsedwith .ord
+ #individual parts that are now .ord are joined again in asc or desc order 
